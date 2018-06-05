4ac05234e1b5385c51949e7b8d0ce34f445a7dff
xbanegas <xbanegas@gmail.com>
2018-06-05 14:21:35 -0400

    SecFooter

--- src/components/SecFooter.js
+++ src/components/SecFooter.js
@@ -2,7 +2,44 @@ import React, { Component } from 'react';
 import '../css/Section.css';
 
 class SecFooter extends Component {
-    render();
+    constructor(props){
+        super(props);
+        this.section = props.secData;
+        this.category = this.section.category || '';
+        this.state = {likes: this.section.likes}
+        this.handleLike = this.handleLike.bind(this);
+    }
+
+    handleLike(e){
+        e.preventDefault();
+        this.setState(prevState => ({
+          likes: prevState.likes += 1
+        }));
+    }
+
+    render() {
+        const category = this.section.category || '';
+        if ((!(category === 'item')) && (!(category === 'quote'))) {
+          return (
+            <div className="sec-foot to-bottom">
+              <a href="#" onClick={this.handleLike} className="btn-like"><i className="fa fa-heart"></i>&nbsp;&nbsp;{this.state.likes}</a>
+              <button className="btn-read">Read Post > </button>
+            </div>
+          );
+        } else if (category === 'quote') {
+          return(
+            <div className="author">
+              {this.section.author}
+            </div>
+          );
+        } else {
+          return ( 
+            <div className="sec-foot to-bottom">
+              <button className="btn-read">Buy Now > </button> 
+            </div>
+          );
+        }
+    };
 }
 
 export default SecFooter;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SecHeader from './SecHeader';
 import '../css/Section.css';
+import SecFooter from './SecFooter';
 
 /**
  * @todo move secFooter and secHeader to their own components 
@@ -10,42 +11,10 @@ class Section extends Component {
   constructor(props){
     super(props);
     this.section = props.secData;
-    this.state = {likes: this.section.likes}
-    this.handleLike = this.handleLike.bind(this);
-    
     console.log(this.section);
   }
 
-  handleLike(e){
-    e.preventDefault();
-    this.setState(prevState => ({
-      likes: prevState.likes += 1
-    }));
-  }
 
-  secFooter() {
-    const category = this.section.category || '';
-    if ((!(category === 'item')) && (!(category === 'quote'))) {
-      return (
-        <div className="sec-foot to-bottom">
-          <a href="#" onClick={this.handleLike} className="btn-like"><i className="fa fa-heart"></i>&nbsp;&nbsp;{this.state.likes}</a>
-          <button className="btn-read">Read Post > </button>
-        </div>
-      );
-    } else if (category === 'quote') {
-      return(
-        <div className="author">
-          {this.section.author}
-        </div>
-      );
-    } else {
-      return ( 
-        <div className="sec-foot to-bottom">
-          <button className="btn-read">Buy Now > </button> 
-        </div>
-      );
-    }
-  }
 
   Description() {
     const hasDescription = this.section.description || false;
@@ -56,14 +25,12 @@ class Section extends Component {
     }
   }
 
-
-
   render() {
     return (
       <section className={`box-${this.section.boxNum}` + ' ' + this.section.align}>
         <SecHeader secData={this.section} />
         {this.Description()}
-        {this.secFooter()}
+        <SecFooter secData={this.section} />
       </section>
     );
   }

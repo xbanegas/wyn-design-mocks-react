2d4f20bbe1d6136931c007bc7942b8554529341c
xbanegas <xbanegas@gmail.com>
2018-06-04 20:43:00 -0400

    working subrow 3

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,23 +4,49 @@ import '../css/Main.css';
 import '../css/Row.css'
 
 class Row extends Component {
-	addSections(){
-    // console.log(this.props);
+  constructor(props){
+    super(props);
+    this.addSections(props);
+  }
+	addSections(props){
+    if (props){
+    let these_props = props.sections.slice(1);
+    console.log(these_props);
     let these_sections = [];
-    this.props.sections.forEach(section => {
-      console.log(section);
+    these_props.forEach(section => {
+      // console.log(section);
       these_sections.push(<Section box={`box-${section.boxNum}`} category={section.category} title={section.title} 
       description={section.description} price={section.price}/>);
     });
-    return these_sections;
+    this.sections = these_sections;
+  }
 	}
 
 	render() {
-		return (
-			<div id={this.props.rowNum}>
-        {this.addSections()}
-			</div>
-		);
+    let rowNum = Number(this.props.rowNum.slice(-1));
+
+    if (rowNum === 3){
+      
+      return(
+        <div id={this.props.rowNum}>
+          <div className="col-1">
+            {this.sections[0]}
+            <div className="sub-row">
+              {this.sections.slice(1,3)}
+            </div>
+          </div>
+          <div className="col-2">
+            {this.sections[3]}
+          </div>
+        </div>
+      );
+    } else {
+		  return (
+			  <div id={this.props.rowNum}>
+          {this.sections}
+			  </div>
+      );
+    }
 	}
 }
 
